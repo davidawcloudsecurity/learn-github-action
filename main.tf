d0aed55f38a77e696195271746b50771f52e1451
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-03-08 16:34:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ terraform {
    }
  }
 }
+
+resource "aws_instance" "my_vm" {
+ ami                       = "ami-07761f3ae34c4478d" //Ubuntu AMI
+ instance_type             = "t2.micro"
+
+ tags = {
+   Name = "test-github-action",
+ }
+}
