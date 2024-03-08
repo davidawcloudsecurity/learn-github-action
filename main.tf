11f22d9573b1413727fb2a8daa932c9001e84cac
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-03-08 17:41:34 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,60 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_vm" {
- ami                       = "ami-07761f3ae34c4478d" //Ubuntu AMI
- instance_type             = "t2.micro"
+locals {
+ ami      = "ami-07761f3ae34c4478d"
+ type     = "t2.micro"
+ name_tag = "test-github-action"
+}
 
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "main_cidrs" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+resource "aws_vpc" "main" {
+ cidr_block = var.main_cidrs
+ 
  tags = {
-   Name = "test-github-action",
+   Name = local.name_tag
+ }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+  
+ tags = {
+   Name = ${local.name_tag}"_Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ 
+ tags = {
+   Name = ${local.name_tag}"_Private Subnet ${count.index + 1}"
  }
 }
