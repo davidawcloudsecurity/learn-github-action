e3e3bcadac63048a25e6d32c9567cf16111dfbff
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-03-08 16:25:12 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
 terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-   }
- }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
 
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
 }
- 
+
 resource "aws_instance" "test_instance" {
- ami           = "ami-07761f3ae34c4478d"
- instance_type = "t2.micro"
- tags = {
-   Name = "test_instance_github_action"
- }
+  ami           = "ami-07761f3ae34c4478d"  # Example AMI ID, replace with your own
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "test_instance_github_action"
+  }
 }
