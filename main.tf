508f36949185d3ac925759b2f98b14c26caaac41
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-03-08 16:37:00 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ terraform {
  }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "my_vm" {
  ami                       = "ami-07761f3ae34c4478d" //Ubuntu AMI
  instance_type             = "t2.micro"
